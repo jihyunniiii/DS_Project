f4e2ed01a145147e8ba794c195d86f3552efdc52
Bedford <qmal789@dgu.ac.kr>
2022-05-03 17:25:09 +0900

    수정

--- Assets/TeamAssets/Script/Character/CharacterSet.cs
+++ Assets/TeamAssets/Script/Character/CharacterSet.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class CharacterSet : MonoBehaviour
 {
-    private enum ControlMode { 
-        Tank, 
+    private enum ControlMode
+    {
+        Tank,
         Direct
     }
 
@@ -43,58 +44,59 @@ public class CharacterSet : MonoBehaviour
         if (!rigidBody) { gameObject.GetComponent<Animator>(); }
     }
     private void OnCollisionEnter(Collision collision)
-    {
-        ContactPoint[] contactPoints = collision.contacts;
-        for (int i = 0; i < contactPoints.Length; i++)
-        {
-            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
-            {
-                if (!collisions.Contains(collision.collider))
-                {
-                    collisions.Add(collision.collider);
-                }
-                isGrounded = true;
-            }
-        }
-    }
-    private void OnCollisionStay(Collision collision)
-    {
-        ContactPoint[] contactPoints = collision.contacts;
-        bool validSurfaceNormal = false;
-        for (int i = 0; i < contactPoints.Length; i++)
-        {
-            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
-            {
-                validSurfaceNormal = true; break;
-            }
-        }
+     {
+         ContactPoint[] contactPoints = collision.contacts;
+         for (int i = 0; i < contactPoints.Length; i++)
+         {
+             if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+             {
+                 if (!collisions.Contains(collision.collider))
+                 {
+                     collisions.Add(collision.collider);
+                 }
+                 isGrounded = true;
+             }
+         }
+     }
+     private void OnCollisionStay(Collision collision)
+     {
+         ContactPoint[] contactPoints = collision.contacts;
+         bool validSurfaceNormal = false;
+         for (int i = 0; i < contactPoints.Length; i++)
+         {
+             if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+             {
+                 validSurfaceNormal = true; break;
+             }
+         }
 
-        if (validSurfaceNormal)
-        {
-            isGrounded = true;
-            if (!collisions.Contains(collision.collider))
-            {
-                collisions.Add(collision.collider);
-            }
-        }
-        else
-        {
-            if (collisions.Contains(collision.collider))
-            {
-                collisions.Remove(collision.collider);
-            }
-            if (collisions.Count == 0) { isGrounded = false; }
-        }
-    }
+         if (validSurfaceNormal)
+         {
+             isGrounded = true;
+             if (!collisions.Contains(collision.collider))
+             {
+                 collisions.Add(collision.collider);
+             }
+         }
+         else
+         {
+             if (collisions.Contains(collision.collider))
+             {
+                 collisions.Remove(collision.collider);
+             }
+             if (collisions.Count == 0) { isGrounded = false; }
+         }
+     }
 
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collisions.Contains(collision.collider))
-        {
-            collisions.Remove(collision.collider);
-        }
-        if (collisions.Count == 0) { isGrounded = false; }
-    }
+     private void OnCollisionExit(Collision collision)
+     {
+
+         if (collisions.Contains(collision.collider))
+         {
+             collisions.Remove(collision.collider);
+         }
+         if (collisions.Count == 0) { isGrounded = false; }
+     }
 
     private void Update()
     {
@@ -111,7 +113,7 @@ public class CharacterSet : MonoBehaviour
         switch (controlMode)
         {
             case ControlMode.Direct:
-               DirectUpdate();
+                DirectUpdate();
                 break;
 
             case ControlMode.Tank:
@@ -210,5 +212,4 @@ public class CharacterSet : MonoBehaviour
             animator.SetTrigger("Jump");
         }
     }
-   
 }
