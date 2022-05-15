1607fc8c871b09809d2ca63c66477632d861dfa3
Bedford <qmal789@dgu.ac.kr>
2022-05-15 23:03:50 +0900

    에셋 구매 추가
    시티와 연등 추가

--- Assets/TeamAssets/Script/TitleScene/LoginField.cs
+++ Assets/TeamAssets/Script/TitleScene/LoginField.cs
@@ -17,10 +17,12 @@ public class LoginField : MonoBehaviour
     public GameObject Signup_text;
     public GameObject login;
     public GameObject nicknametext;
+    public GameObject Majorobject;
     public InputField idInput;
     public InputField pwInput;
     public InputField Certify_num;
     public InputField userNicknameInput;
+    public InputField Major;
 
     int ran;
     string email = "dgu.ac.kr";
@@ -28,6 +30,7 @@ public class LoginField : MonoBehaviour
     void Start()
     {
         var bro = Backend.Initialize(true);
+       
         if (bro.IsSuccess())
         {
             Debug.Log("초기화 성공");
@@ -51,7 +54,7 @@ public class LoginField : MonoBehaviour
             if (bro.IsSuccess())
             {
                 Debug.Log("로그인에 성공했습니다");
-                GameObject.FindWithTag("FadeController").GetComponent<FadeInOut>().FadeToNext();
+                //GameObject.FindWithTag("FadeController").GetComponent<FadeInOut>().FadeToNext();
             }
         }
         else {
@@ -62,58 +65,66 @@ public class LoginField : MonoBehaviour
     public void Certify() {
         if (idInput.text.Contains(email) && pwInput.text.Length != 0)
         {
-            id.SetActive(false);
-            pw.SetActive(false);
-            Certify_number.SetActive(true);
-            Signup.SetActive(true);
-            Signup_text.SetActive(false);
-            login.SetActive(false);
-            nicknametext.SetActive(true);
-            ran = Random.Range(100000, 999999);
+            BackendReturnObject bro = Backend.BMember.CustomLogin(idInput.text, pwInput.text);
+            if (bro.IsSuccess())
+            {
+                Debug.Log("이미 가입된 아이디입니다.");
+                Backend.BMember.Logout();
+            }
+            else
+            {
+                id.SetActive(false);
+                pw.SetActive(false);
+                Certify_number.SetActive(true);
+                Signup.SetActive(true);
+                Signup_text.SetActive(false);
+                login.SetActive(false);
+                nicknametext.SetActive(true);
+                Majorobject.SetActive(true);
+                ran = Random.Range(100000, 999999);
 
-            MailMessage mail = new MailMessage();
-            mail.From = new MailAddress("qmal789@dgu.ac.kr"); // 보내는사람
-            mail.To.Add(idInput.text); // 받는 사람
-            mail.Subject = "\"동심\"에서 회원가입 인증 메일을 보냈습니다.";
-            mail.Body = "인증 번호 : " + ran + "\n 게임으로 돌아가 화면에 인증번호를 입력하세요.";
-            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
-            smtpServer.Port = 587;
-            smtpServer.Credentials = new System.Net.NetworkCredential("qmal789@dgu.ac.kr", "anzm5623!") as ICredentialsByHost; // 보내는사람 주소 및 비밀번호 확인
-            smtpServer.EnableSsl = true;
-            ServicePointManager.ServerCertificateValidationCallback =
-            delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-            { return true; };
-            smtpServer.Send(mail);
-            Debug.Log("success");
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("qmal789@dgu.ac.kr"); // 보내는사람
+                mail.To.Add(idInput.text); // 받는 사람
+                mail.Subject = "\"동심\"에서 회원가입 인증 메일을 보냈습니다.";
+                mail.Body = "인증 번호 : " + ran + "\n 게임으로 돌아가 화면에 인증번호를 입력하세요.";
+                SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+                smtpServer.Port = 587;
+                smtpServer.Credentials = new System.Net.NetworkCredential("qmal789@dgu.ac.kr", "anzm5623!") as ICredentialsByHost; // 보내는사람 주소 및 비밀번호 확인
+                smtpServer.EnableSsl = true;
+                ServicePointManager.ServerCertificateValidationCallback =
+                delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+                { return true; };
+                smtpServer.Send(mail);
+                Debug.Log("success");
+            }
         }
     }
     public void Signin() {
         if (idInput.text.Contains(email))
         {
-            Debug.Log(ran);
-            if (ran == int.Parse(Certify_num.text))
+            if (ran == int.Parse(Certify_num.text) && Certify_num.text.Length != 0 && Major.text.Length != 0 && userNicknameInput.text.Length !=0)
             {
-                id.SetActive(true);
-                pw.SetActive(true);
-                Certify_number.SetActive(false);
-                Signup.SetActive(false);
-                Signup_text.SetActive(true);
-                login.SetActive(true);
-                nicknametext.SetActive(false);
+                
                 BackendReturnObject bro = Backend.BMember.CustomSignUp(idInput.text, pwInput.text);
                 if (bro.IsSuccess())
                 {
                     Debug.Log("CustomSignUp : " + bro);
-
                     Debug.Log("닉네임 업데이트" + Backend.BMember.UpdateNickname(userNicknameInput.text));
-
-         
                     Backend.BMember.UpdateCustomEmail(idInput.text);
                     Debug.Log("회원가입에 성공했습니다");
+                    id.SetActive(true);
+                    pw.SetActive(true);
+                    Certify_number.SetActive(false);
+                    Signup.SetActive(false);
+                    Signup_text.SetActive(true);
+                    login.SetActive(true);
+                    nicknametext.SetActive(false);
+                    Majorobject.SetActive(false);
                 }
             }
             else {
-                Debug.Log("인증 번호가 다릅니다.");
+                Debug.Log("인증 번호가 다르거나 빈칸이 있습니다.");
             }
         }
         else {
