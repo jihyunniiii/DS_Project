129d741b380cfbd18a9eea4e40258dc13eeea967
Bedford <qmal789@dgu.ac.kr>
2022-05-15 20:49:40 +0900

    Revert "Merge branch '0515_main'"
    This reverts commit 452039c1365641a89b26d918bc7ee9aa82c22eb4, reversing
    changes made to 92bddd6c0c5f6962206204a0dc55e2212977649b.

--- Assets/TeamAssets/Script/LobbyScene/lobby.cs
+++ Assets/TeamAssets/Script/LobbyScene/lobby.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 using UnityEngine.Audio;
 using UnityEngine.UI;
-using TMPro;
-using UnityEngine.SceneManagement;
 
 public class lobby : MonoBehaviour
 {
@@ -14,37 +12,19 @@ public class lobby : MonoBehaviour
     public GameObject SettingSound;
     public GameObject RankUI;
 
+    public AudioMixer masterMixer;
     public Slider audioSlider;
-    public AudioSource audioSource;
-
-    public GameObject StoreBox;
-    public GameObject StoreUI;
-    public Camera getCamera;
-    private RaycastHit hit;
-
-    public TextMeshProUGUI CoinTxt;
-    public TextMeshProUGUI LanternTxt;
-    public int Coin;
-    public int Lantern;
-
-    public GameObject GameRoomBox;
-
-
+   
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
         UI.gameObject.SetActive(false);
-        Coin = 10;
-        Lantern = 0;
-        CoinTxt.text = ": " + Coin.ToString();
-        LanternTxt.text = ": " + Lantern.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (StoreUI.activeSelf == false && Input.GetKeyUp(KeyCode.Escape))
+        if (Input.GetKeyUp(KeyCode.Escape))
         {
             if (UI.activeSelf == true)
             {
@@ -56,52 +36,18 @@ public class lobby : MonoBehaviour
                 UI.gameObject.SetActive(true);
             }
         }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = getCamera.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                // string objectName = hit.collider.gameObject.name;
-                
-                // Store UI 활성화
-                if (hit.collider.gameObject == StoreBox && SettingSound.activeSelf == false && UI.activeSelf == false && SettingUI.activeSelf == false)
-                {
-                    Debug.Log("상점에 들어와 물품을 구매합니다.");
-                    StoreUI.gameObject.SetActive(true);
-                    CoinTxt.text = ": " + Coin.ToString();
-                    LanternTxt.text = ": " + Lantern.ToString();
-                }
-
-                if (hit.collider.gameObject == GameRoomBox && SettingSound.activeSelf == false && UI.activeSelf == false && SettingUI.activeSelf == false && StoreUI.activeSelf == false)
-                {
-                    Debug.Log("게임룸으로 이동합니다.");
-                    SceneManager.LoadScene("GameRoom");
-                }
-            }
-        }
     }
 
     public void SettingUIOn()
     {
-        if (SettingSound.activeSelf == true && UI.activeSelf == false)
+        if (UI.activeSelf == true)
         {
-            SettingSound.gameObject.SetActive(false);
-            UI.gameObject.SetActive(true);
+            SettingUI.gameObject.SetActive(true);
+            UI.gameObject.SetActive(false);
         }
     }
 
     public void SettingSoundOn()
-    {
-        if (UI.activeSelf == true)
-        {
-            UI.gameObject.SetActive(false);
-            SettingSound.gameObject.SetActive(true);
-        }
-    }
-
-    public void SettingSoundClick()
     {
         if (SettingUI.activeSelf == true && UI.activeSelf == false)
         {
@@ -138,16 +84,11 @@ public class lobby : MonoBehaviour
             RankUI.gameObject.SetActive(false);
             UI.gameObject.SetActive(true);
         }
-
-        else if (StoreUI == true)
-        {
-            StoreUI.gameObject.SetActive(false);
-        }
     }
 
-    public void AudioControl()
+    public void AudioControl(float sliderVal)
     {
-        audioSource.volume = audioSlider.value;
+        masterMixer.SetFloat("Music", Mathf.Log10(sliderVal) * 20);
     }
 
     public void Camera()
@@ -171,22 +112,6 @@ public class lobby : MonoBehaviour
         {
             RankUI.gameObject.SetActive(true);
             UI.gameObject.SetActive(false);
-        }
-    }
-
-    public void LanternBuy()
-    {
-        if (Coin >= 5)
-        {
-            Debug.Log("연등 1개를 구매하였습니다.");
-            Coin = Coin - 5;
-            Lantern = Lantern + 1;
-            CoinTxt.text = ": " + Coin.ToString();
-            LanternTxt.text = ": " + Lantern.ToString();
-        }
-        else
-        {
-            Debug.Log("코인이 부족하여 연등을 구매할 수 없습니다.");
         }
     }
 }
