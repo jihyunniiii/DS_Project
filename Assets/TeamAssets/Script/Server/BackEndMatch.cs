32b1fc1790b8e209ce461091e8e1ad6f36992292
Bedford <qmal789@dgu.ac.kr>
2022-05-18 01:24:25 +0900

    매칭 서버 구현 완료
    매칭이 되긴하나 혼자서는 성공을 확인할 수 없음...
    튼 매칭이 되긴함

--- Assets/TeamAssets/Script/Server/BackEndMatch.cs
+++ Assets/TeamAssets/Script/Server/BackEndMatch.cs
@@ -1,24 +1,33 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using BackEnd;
 using BackEnd.Tcp;
 using System.Linq;
+using LitJson;
+
+using static BackEnd.SendQueue;
 
 
 public partial class BackEndMatch : MonoBehaviour
 {
-    public class MatchInfo
-    {
-        public string title;                // 매칭 명
-        public string inDate;               // 매칭 inDate (UUID)
-        public MatchType matchType;         // 매치 타입
-        public MatchModeType matchModeType; // 매치 모드 타입
-        public string headCount;            // 매칭 인원
-        public bool isSandBoxEnable;        // 샌드박스 모드 (AI매칭)
-    }
+    private string inGameRoomToken = string.Empty;  // 게임 룸 토큰 (인게임 접속 토큰)
+    public SessionId hostSession { get; private set; }  // 호스트 세션
+    public bool isSandBoxGame { get; private set; } = false;
+
     private static BackEndMatch instance = null; // 인스턴스
     public GameObject loadingUI;
+    public GameObject matchUI;
+    public GameObject matchbutton;
+    public GameObject matchbuttoncancel;
+    public Text nickName;
+    string mynickName;
+    string myIndate;
+    string indate;
+    
+
+
     private void Awake()
     {
         if (instance != null)
@@ -31,7 +40,11 @@ public partial class BackEndMatch : MonoBehaviour
     void Start()
     {
         joinmatchserver();
+        OnBackendUserInfo();
+        SetNickName();
         Invoke("createMatch", 1f);
+        checkBackEnd();
+        
     }
 
     // Update is called once per frame
@@ -63,4 +76,51 @@ public partial class BackEndMatch : MonoBehaviour
             Debug.Log("대기방이 생성되었습니다.");
         };
     }
+    void OnBackendUserInfo() {
+        BackendReturnObject bro = Backend.BMember.GetUserInfo();
+        mynickName = bro.GetReturnValuetoJSON()["row"]["nickname"].ToString();
+    }
+
+    void SetNickName() {
+        nickName.text = mynickName;
+    }
+
+    public void MacthingGame() {
+        var bro = Backend.Match.GetMatchList();
+        JsonData matchCardListJson = bro.FlattenRows();
+        indate = matchCardListJson[0]["inDate"].ToString();
+        Backend.Match.RequestMatchMaking(MatchType.Random,MatchModeType.Melee, indate);
+    }
+
+    public void MatcingGameCancel() {
+        Backend.Match.CancelMatchMaking();
+    }
+
+    void  checkBackEnd() {
+        Backend.Match.OnMatchMakingResponse = (MatchMakingResponseEventArgs args) => {
+            // TODO
+            switch (args.ErrInfo) {
+                case ErrorCode.Success:
+                    Debug.Log("매칭에 성공하였습니다.");
+                    loadingUI.SetActive(false);
+                    matchUI.SetActive(true);
+                    break;
+                case ErrorCode.Match_InProgress:
+                    if (args.Reason == String.Empty) {
+                        Debug.Log("매치 신청 성공");
+                        loadingUI.SetActive(true);
+                        matchbutton.SetActive(false);
+                        matchbuttoncancel.SetActive(true);
+                    }
+                    break;
+                case ErrorCode.Match_MatchMakingCanceled:
+                    Debug.Log("매칭 신청 중단");
+                    loadingUI.SetActive(false);
+                    matchbutton.SetActive(true);
+                    matchbuttoncancel.SetActive(false);
+                    break;
+            }
+        };
+    }
+   
 }
