3bd35a1b0294e1ffffa215fa2f31de3a0509ba02
Bedford <qmal789@dgu.ac.kr>
2022-05-23 18:34:57 +0900

    중간 에니메이션 저장

--- Assets/TeamAssets/Script/Character/Getname.cs
+++ Assets/TeamAssets/Script/Character/Getname.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using BackEnd;
 using TMPro;
-public class Getname : MonoBehaviour
+using Unity.Netcode;
+public class Getname : NetworkBehaviour
 {
-    public TextMeshProUGUI textMeshPro;
+    private NetworkVariable<NetworkString> playersName = new NetworkVariable<NetworkString>();
+
+    private bool overlaySet = false;
     // Start is called before the first frame update
-    void Start()
+  
+    public override void OnNetworkSpawn()
     {
-        BackendReturnObject bro = Backend.BMember.GetUserInfo();
-        textMeshPro.text = bro.GetReturnValuetoJSON()["row"]["nickname"].ToString();
+        if (IsServer) {
+            BackendReturnObject bro = Backend.BMember.GetUserInfo();
+            playersName.Value = bro.GetReturnValuetoJSON()["row"]["nickname"].ToString();
+        }
+    }
+
+    public void SetOverlay() { 
+        var localPlayerOverlay = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        localPlayerOverlay.text = playersName.Value;
+    }
+
+    private void Update()
+    {
+        if (!overlaySet && !string.IsNullOrEmpty(playersName.Value)) { 
+            SetOverlay();
+            overlaySet = true;
+        }
     }
 }
