73a176d1adeaec733c187998db184431094f81e8
Bedford <qmal789@dgu.ac.kr>
2022-05-26 00:51:53 +0900

    연등 동기화 완료 플레이어 닉네임 실패

--- Assets/TeamAssets/Script/Character/Getname.cs
+++ Assets/TeamAssets/Script/Character/Getname.cs
@@ -9,16 +9,13 @@ public class Getname : NetworkBehaviour
     private NetworkVariable<NetworkString> playersName = new NetworkVariable<NetworkString>();
 
     private bool overlaySet = false;
+    //private bool isonetimeset = false;
     // Start is called before the first frame update
   
     public override void OnNetworkSpawn()
     {
-        if (IsServer) {
-            BackendReturnObject bro = Backend.BMember.GetUserInfo();
-            playersName.Value = bro.GetReturnValuetoJSON()["row"]["nickname"].ToString();
-        }
+        if (IsServer) { playersName.Value = $"Player {OwnerClientId}"; }
     }
-
     public void SetOverlay() { 
         var localPlayerOverlay = gameObject.GetComponentInChildren<TextMeshProUGUI>();
         localPlayerOverlay.text = playersName.Value;
--- Assets/TeamAssets/Script/Character/PlayerControl.cs
+++ Assets/TeamAssets/Script/Character/PlayerControl.cs
@@ -1,9 +1,8 @@
-using System;
 using Unity.Netcode;
 using UnityEngine;
-
+using DilmerGames.Core.Singletons;
 [RequireComponent(typeof(NetworkObject))]
-public class PlayerControl : NetworkBehaviour
+public class PlayerControl : NetworkSingleton<PlayerControl>
 {
     #region Variables
     //basic variables about the movements
@@ -20,8 +19,14 @@ public class PlayerControl : NetworkBehaviour
     private Vector3 drags;
     [SerializeField]
     private LayerMask ground;
+    [SerializeField]
+    private GameObject objectPrefabLantern;
 
-    public Transform t;
+    private NetworkVariable<NetworkString> playersName = new NetworkVariable<NetworkString>();
+
+    private bool overlaySet = false;
+    private bool isonetimeset = false;
+
     //to get CharacterController from the unity
     private CharacterController characterController;
     [SerializeField]
@@ -38,7 +43,6 @@ public class PlayerControl : NetworkBehaviour
     private Vector3 calcVelocity = Vector3.zero;
 
     //movement(vector3)
-    private Vector3 inputDirection = Vector3.zero;
 
     //prevent double jump
     [SerializeField]
@@ -68,24 +72,45 @@ public class PlayerControl : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        ClientMoveingtoserver();
-        Client_visual();
+        
         if (IsClient && IsOwner)
         {
             ClientMove();
             LanternSetting();
         }
+        ClientMoveingtoserver();
+        Client_visual();
     }
 
     public void LanternSetting()
     {
+        
         if (Input.GetKeyDown(KeyCode.R))
         {
-            SpawnControl.Instance.SpawnLantern(transform);
-            Debug.Log("실행");
+            if (IsServer)
+            {
+                Spawn();
+            }
+            else if (IsClient && IsOwner)
+            {
+                SpawnServerRpc();
+            }
+
         }
     }
-
+    public void Spawn()
+    {
+        Vector3 temp;
+        Vector3 t = new Vector3(0, 1.5f, 0);
+        temp = transform.position - t;
+        GameObject go = Instantiate(objectPrefabLantern, temp, Quaternion.identity);
+        go.GetComponent<NetworkObject>().Spawn();
+    }
+    [ServerRpc]
+    public void SpawnServerRpc()
+    {
+        Spawn();
+    }
     private void Client_visual()
     {
         if (oldPlayerState != networkPlayerState.Value && networkPlayerState.Value != PlayerState.Jump && networkPlayerState.Value != PlayerState.Ground)
@@ -140,8 +165,8 @@ public class PlayerControl : NetworkBehaviour
             move = move * runSpeedOffset;
             UpdatePlayerStateServerRpc(PlayerState.Run);
         }
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded )
         {
             calcVelocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
             UpdatePlayerStateServerRpc(PlayerState.Jump);
@@ -182,191 +207,12 @@ public class PlayerControl : NetworkBehaviour
         // 약간 신체에 박혀 있는 위치로부터 발사하지 않으면 제대로 판정할 수 없을 때가 있다.
         var ray = new Ray(this.transform.position + Vector3.up * 0.1f, Vector3.down);
         // 탐색 거리
-        var maxDistance = 0.1f;
+        var maxDistance = 0.15f;
         // 광선 디버그 용도
         Debug.DrawRay(transform.position + Vector3.up * 0.1f, Vector3.down * maxDistance, Color.red);
         // Raycast의 hit 여부로 판정
         // 지상에만 충돌로 레이어를 지정
         return Physics.Raycast(ray, maxDistance, ground);
     }
+    
 }
-/*using Unity.Netcode;
-using UnityEngine;
-
-[RequireComponent(typeof(NetworkObject))]
-public class PlayerControl : NetworkBehaviour
-{
-    [SerializeField]
-    private float walkSpeed = 2.0f;
-
-    [SerializeField]
-    private float runSpeedOffset = 1.5f;
-
-
-    [SerializeField]
-    private Vector2 defaultInitialPositionOnPlane = new Vector2(-4, 4);
-
-    [SerializeField]
-    private NetworkVariable<Vector3> networkPositionDirection = new NetworkVariable<Vector3>();
-    [SerializeField]
-    private NetworkVariable<Vector3> networkPositionDirectionY = new NetworkVariable<Vector3>();
-    [SerializeField]
-    private NetworkVariable<Quaternion> networkRotationDirection = new NetworkVariable<Quaternion>();
-
-    [SerializeField]
-    private NetworkVariable<PlayerState> networkPlayerState = new NetworkVariable<PlayerState>();
-
-    public bool IsJumping;
-    public float JumpPower;
-
-    private CharacterController CharacterController;
-    // client caches positions
-    private Vector3 oldInputPosition = Vector3.zero;
-    private Vector3 oldInputRotation = Vector3.zero;
-    private PlayerState oldPlayerState = PlayerState.Idle;
-    Vector3 moveDirection = Vector3.zero;
-    private float currentV = 0;
-    private float currentH = 0;
-    private readonly float interpolation = 10;
-    private Vector3 currentDirection = Vector3.zero;
-    private Animator animator;
-
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
-        CharacterController = GetComponent<CharacterController>();
-        IsJumping = false;
-    }
-
-    void Start()
-    {
-        if (IsClient && IsOwner)
-        {
-            transform.position = new Vector3(Random.Range(defaultInitialPositionOnPlane.x, defaultInitialPositionOnPlane.y), 0,
-                   Random.Range(defaultInitialPositionOnPlane.x, defaultInitialPositionOnPlane.y));
-        }
-    }
-
-    void FixedUpdate()
-    {
-        ClientMoveAndRotate();
-        ClientVisuals();
-        if (IsClient && IsOwner)
-        {
-            ClientInput();
-            if (moveDirection.y == 0)
-                UpdatePlayerStateServerRpc(PlayerState.Ground);
-        }
-    }
-
-    private void ClientMoveAndRotate()
-    {
-
-        transform.position += networkPositionDirection.Value;
-        if (networkPositionDirectionY.Value != Vector3.zero)
-            CharacterController.Move(networkPositionDirectionY.Value);
-        transform.rotation = networkRotationDirection.Value;
-
-    }
-
-    private void ClientVisuals()
-    {
-        if (oldPlayerState != networkPlayerState.Value && networkPlayerState.Value != PlayerState.Jump && networkPlayerState.Value != PlayerState.Ground)
-        {
-            oldPlayerState = networkPlayerState.Value;
-            animator.SetTrigger($"{networkPlayerState.Value}");
-        }
-        if (oldPlayerState != networkPlayerState.Value && networkPlayerState.Value == PlayerState.Jump && networkPlayerState.Value != PlayerState.Ground)
-        {
-            oldPlayerState = networkPlayerState.Value;
-            animator.SetBool("Jump", true);
-        }
-        if (oldPlayerState != networkPlayerState.Value && networkPlayerState.Value == PlayerState.Ground && networkPlayerState.Value != PlayerState.Jump)
-        {
-            oldPlayerState = networkPlayerState.Value;
-            animator.SetBool("Jump", false);
-        }
-
-    }
-
-    private void ClientInput()
-    {
-        // left & right rotation
-        Vector3 inputRotation = new Vector3(0, Input.GetAxis("Horizontal"), 0);
-
-        // forward & backward direction
-        //Vector3 direction = transform.TransformDirection(Vector3.forward);
-        float forwardInput = Input.GetAxis("Vertical");
-        //Vector3 inputPosition = direction * forwardInput;
-        Vector3 inputPosition = Vector3.zero;
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
-        Transform camera = Camera.main.transform;
-        currentV = Mathf.Lerp(currentV, v, Time.deltaTime * interpolation);
-        currentH = Mathf.Lerp(currentH, h, Time.deltaTime * interpolation);
-        Vector3 direction = camera.forward * currentV + camera.right * currentH;
-        float directionLength = direction.magnitude;
-        direction.y = 0;
-        direction = direction.normalized * directionLength;
-
-        if (direction != Vector3.zero)
-        {
-            currentDirection = Vector3.Slerp(currentDirection, direction, Time.deltaTime * interpolation);
-            inputPosition = currentDirection;
-        }
-        // change animation states
-        if (inputPosition == Vector3.zero)
-            UpdatePlayerStateServerRpc(PlayerState.Idle);
-        else if (!ActiveRunningActionKey() && inputPosition != Vector3.zero)
-            UpdatePlayerStateServerRpc(PlayerState.Walk);
-        else if (ActiveRunningActionKey() && inputPosition != Vector3.zero)
-        {
-            inputPosition = currentDirection * runSpeedOffset;
-            UpdatePlayerStateServerRpc(PlayerState.Run);
-        }
-
-        Jump();
-        if (oldInputPosition != inputPosition ||
-            oldInputRotation != inputRotation)
-        {
-            oldInputPosition = inputPosition;
-
-            UpdateClientPositionAndRotationServerRpc(inputPosition * walkSpeed * Time.deltaTime, Quaternion.LookRotation(currentDirection), moveDirection);
-        }
-    }
-
-    private void Jump()
-    {
-        if (CharacterController.isGrounded)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                moveDirection.y = JumpPower;
-                UpdatePlayerStateServerRpc(PlayerState.Jump);
-            }
-        }
-        else
-        {
-            moveDirection.y -= JumpPower * Time.deltaTime;
-        }
-    }
-    private static bool ActiveRunningActionKey()
-    {
-        return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-    }
-
-    [ServerRpc]
-    public void UpdateClientPositionAndRotationServerRpc(Vector3 newPosition, Quaternion newRotation, Vector3 y)
-    {
-        networkPositionDirection.Value = newPosition;
-        networkRotationDirection.Value = newRotation;
-        networkPositionDirectionY.Value = y;
-    }
-
-    [ServerRpc]
-    public void UpdatePlayerStateServerRpc(PlayerState state)
-    {
-        networkPlayerState.Value = state;
-    }
-}
-*/
