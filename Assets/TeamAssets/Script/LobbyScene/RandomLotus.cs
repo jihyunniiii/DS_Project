039107890b394945f3c3019813369917c2e8441d
chaening <kce2360@gmail.com>
2022-05-26 15:16:41 +0900

    Revert "Merge branch '0526'"
    This reverts commit 8242a20c5547bb0cb5c7a53275ed9b73e62d988a, reversing
    changes made to 73a176d1adeaec733c187998db184431094f81e8.

--- Assets/TeamAssets/Script/LobbyScene/RandomLotus.cs
+++ Assets/TeamAssets/Script/LobbyScene/RandomLotus.cs
@@ -1,137 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class RandomLotus : MonoBehaviour
 {
-
     public GameObject LotusPrefab;
 
     public int count; //생성되는 연꽃 개수
     public float TimeSpan; // 경과 시간
     public int CheckTime;// 특정 시간
 
-    public GameObject[] L_count;//획득한 연꽃의 수를 세기 위한 배열 선언
-    public int len = 0;//배열의 길이 확인
-
-
-    public GameObject lotusUI;
-    public TextMeshProUGUI lotusTxt;
-
-    public GameObject wordUI;
-    public TextMeshProUGUI wordTxt;
-    public Button Wordbtn;
-
-    string word = "타인이 너를 무엇으로 생각하는지는 중요치 않다. 스스로 생각하는 너 자신이 너의 모든 것이다.";
-    public string[] TodayWord;//단어마다 저장하기 위한 배열
-    public int letter;//랜덤으로 선택될 배열 index
-    public bool A ;//button 활성화,비활성화 위함
-
-
-
     void Start()
-    {   
-        
-        A = false;
+    {
         TimeSpan = 0.0f;
         CheckTime = Random.Range(5, 10);
 
         Invoke("Spawn", CheckTime);
-
-        InvokeRepeating("phrase", 10, 60);
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (wordUI.activeSelf == false)
-        {
-            A = false;
-        }
-        else if(wordUI.activeSelf ==true)
-            A = true;
-
         TimeSpan += Time.deltaTime;
-        DestroyLotus();
 
     }
 
-    public void Spawn()
+    private void Spawn()
     {
-        count = Random.Range(5, 8);
+        count = Random.Range(2, 7);
+        Debug.Log("ChechTime" + Mathf.Round(CheckTime));
+        Debug.Log("count" + Mathf.Round(count));
 
-
-        if (TimeSpan > CheckTime)
+        if(TimeSpan>CheckTime)
         {
-            for (int i = 0; i < count; i++)
+            for(int i=0;i<count;i++)
             {
                 SpawnPosition();
             }
             TimeSpan = 0.0f;
-
         }
-
-
-    }
-
-    public void SpawnPosition()
-    {
-        GameObject selectedPrefab = LotusPrefab;
-
-        Vector3 spawnPos = new Vector3(Random.Range(-300, -170), 20, Random.Range(-60, 60));
-
-        GameObject instance = Instantiate(selectedPrefab, spawnPos, Quaternion.identity);
-
-
-    }
-
-    public void DestroyLotus()
-    {
-        L_count = GameObject.FindGameObjectsWithTag("lotus");//생성된 연꽃을 배열에 담음
-        len = L_count.Length;//남아있는 연꽃의 개수 
-
-
-        for (int i = 1; i < count + 1; i++)
-        {
-            if (len == count - i)
-            {
-                lotusTxt.text = ": " + i.ToString();
-
-                
-                if (i != 0 && i % 5 == 0 && A==false )
-                {
-                    
-                    wordTxt.text = TodayWord[letter].ToString();
-
-                    wordUI.SetActive(true);
-
-                    A = true;
-                }
-
-            }
-        }
-    }
-
-    public void phrase()
-    {
-        TodayWord = word.Split(new char[] { ' ' });
-        letter = Random.Range(0, TodayWord.Length);
-    }
-
-    public void button()
-    {
-        if(A==true)
-        {
-            wordUI.SetActive(false);
-            A = false;
-        }
-        
     }
    
+    private void SpawnPosition()
+    {   
+        GameObject selectedPrefab = LotusPrefab;
+
+        Vector3 spawnPos = new Vector3(Random.Range(-45, 45), 20, Random.Range(-45, 45));
+
+        GameObject instance = Instantiate(selectedPrefab, spawnPos, Quaternion.identity);
+        
+
+    }
 }
-
-
-
