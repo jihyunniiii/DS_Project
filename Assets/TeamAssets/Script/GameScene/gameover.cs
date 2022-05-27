bbafe647f38d855019600f7e3d2a4b9374befe61
yeaahhh <75093565+kimdaye1223@users.noreply.github.com>
2022-05-28 04:42:49 +0900

    수정

--- Assets/TeamAssets/Script/GameScene/gameover.cs
+++ Assets/TeamAssets/Script/GameScene/gameover.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class gameover : MonoBehaviour
 {
     public GameObject GameOver;
+    public GameObject tile;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class gameover : MonoBehaviour
         if(col.gameObject.tag == "Player")
         {
             GameOver.SetActive(true);
+            Destroy(tile);
         }
     }
 }
